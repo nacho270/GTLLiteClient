fc8e4894062bc976d471f5db438cf30d81a79c82
nacho <ignaciocicero@gmail.com>
2016-10-06 20:09:52 -0300

    Algo que puede ser util

--- src/main/GuiBackGTLLite.java
+++ src/main/GuiBackGTLLite.java
@@ -12,31 +12,43 @@ public class GuiBackGTLLite extends GTLLiteClientMainTemplate {
 
 	protected GuiBackGTLLite(int idAplicacion, String version) throws FWException {
 		super(idAplicacion, version);
-		getDesktop().setBackground(new Color(255,255,255));
+		getDesktop().setBackground(new Color(255, 255, 255));
 		setBackgroundImage("ar/com/lite/textillevel/imagenes/logogtl.jpg");
 		setIconoVentana("ar/com/lite/textillevel/imagenes/logogtl-ventana.jpg");
-		
-		//oculto el item cambiar usuario xq no hay login
+
+		/*
+		 * Asi se puede agregar un icono al desktop pane... puede ser util
+		 * JButton button = new JButton("prueba",
+		 * UIManager.getIcon("OptionPane.informationIcon"));
+		 * button.setVerticalTextPosition(SwingConstants.BOTTOM);
+		 * button.setHorizontalTextPosition(SwingConstants.CENTER);
+		 * button.setBounds(new Rectangle(new Point(5, 5),
+		 * button.getPreferredSize())); getDesktop().add(button);
+		 */
+
+		// oculto el item cambiar usuario xq no hay login
 		ManejadorMenues.getManejadorMenues(this).getMenuDefault().getMenuItemCambiarUsuario().setVisible(false);
-		
+
 	}
 
 	public static void main(String[] args) {
 		try {
 			System.getProperties().setProperty("applicationName", "GTL");
-			System.getProperties().setProperty("java.naming.factory.initial", "org.jnp.interfaces.NamingContextFactory");
+			System.getProperties().setProperty("java.naming.factory.initial",
+					"org.jnp.interfaces.NamingContextFactory");
 			System.getProperties().setProperty("java.naming.factory.url.pkgs", "org.jboss.naming:org.jnp.interfaces");
-			System.getProperties().setProperty("cltimezone","GMT-3");
-			if(System.getProperty("java.naming.provider.url_otro_sistema")==null){
+			System.getProperties().setProperty("cltimezone", "GMT-3");
+			if (System.getProperty("java.naming.provider.url_otro_sistema") == null) {
 				System.getProperties().setProperty("java.naming.provider.url_otro_sistema", "localhost:1099");
 			}
-			if(System.getProperty("java.naming.provider.url")==null){
+			if (System.getProperty("java.naming.provider.url") == null) {
 				System.getProperties().setProperty("java.naming.provider.url", "localhost:1099");
 			}
-			
+
 			GuiBackGTLLite guiBackTextilLevel = new GuiBackGTLLite(-1, VERSION);
-//			EventQueue queue = Toolkit.getDefaultToolkit().getSystemEventQueue();
-//			queue.push(new EventQueueProxy(guiBackTextilLevel));
+			// EventQueue queue =
+			// Toolkit.getDefaultToolkit().getSystemEventQueue();
+			// queue.push(new EventQueueProxy(guiBackTextilLevel));
 			guiBackTextilLevel.setExtendedState(MAXIMIZED_BOTH);
 			guiBackTextilLevel.iniciarAplicacion();
 		} catch (Exception e) {
